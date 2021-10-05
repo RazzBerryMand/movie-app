8abf6e0aacf221d76fd775db74045da3f658f570
Mand Cashin <amanda.cashin@magairports.com>
2021-10-05 10:32:15 +0100

    styles: improve styles for movie cards

--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,20 @@
             :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
             alt=""
           />
-          <p class="title">{{ movie.title }}</p>
-          <p class="release">
-            Released:
-            {{
-              new Date(movie.release_date).toLocaleString('en-us', {
-                month: 'long',
-                day: 'numeric',
-                year: 'numeric',
-              })
-            }}
-          </p>
-          <p class="rating">Rating: {{ movie.vote_average }}</p>
+          <div class="movie-info">
+            <p class="title">{{ movie.title }}</p>
+            <p class="release">
+              Released:
+              {{
+                new Date(movie.release_date).toLocaleString('en-us', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })
+              }}
+            </p>
+            <p class="rating">Rating: {{ movie.vote_average }}</p>
+          </div>
           <NuxtLink
             :to="{ name: 'movies-movieid', params: { movieid: movie.id } }"
             class="button"
@@ -62,22 +64,33 @@ export default {
 
 <style lang="scss">
 .movies {
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: calc(var(--spacing-base) * 2);
   color: var(--text-white);
 
   .movie-grid {
     display: grid;
     column-gap: calc(var(--spacing-base) * 2);
     row-gap: calc(var(--spacing-base) * 4);
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
+    
+    @media only screen and (min-width: 768px) {
+      grid-template-columns: repeat(4, 1fr);
+    }
 
     .movie-card {
       display: flex;
       flex-direction: column;
+      background: var(--bg-secondary);
 
       img {
-        display: block;
         width: 100%;
         height: 100%;
+      }
+
+      .movie-info {
+        margin: var(--spacing-base);
       }
     }
   }
