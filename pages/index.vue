58acb6010d72f27d774088229212ebc5a356d7a2
Mand Cashin <amanda.cashin@magairports.com>
2021-10-05 09:50:41 +0100

    feat: api call

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,28 @@
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      movies: [],
+    }
+  },
+
+  async fetch() {
+    await this.getMovies()
+  },
+
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=114888fa1dd2d3f986b639d6bb97e22c&language=en-US&page=1`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+    },
+  },
+}
 </script>
